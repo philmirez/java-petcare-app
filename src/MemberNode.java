56275e3e5773ee1d006ff56fcd2f040440ff5920
usman-tahir <tahir.usman.ali94@outlook.com>
2016-11-08 13:11:33 -0500

    added a MemberNode class for Member linking (will be used alongside the Member reports)

---
+++ src/MemberNode.java
@@ -0,0 +1,42 @@
+
+public class MemberNode {
+    private Member member;
+    private MemberNode link;
+
+    public MemberNode() {
+        this.member = new Member();
+        this.link = null;
+    }
+
+    public MemberNode(Member member, MemberNode link) {
+        this.member = member;
+        this.link = link;
+    }
+
+    public Member getMember() {
+        return this.member;
+    }
+
+    public MemberNode getLink() {
+        return this.link;
+    }
+
+    public String getMembderData() {
+        return this.member.toString();
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public void setLink(MemberNode link) {
+        this.link = link;
+    }
+
+    public boolean hasNext() {
+        if (this.link == null) {
+            return false;
+        }
+        return true;
+    }
+}
