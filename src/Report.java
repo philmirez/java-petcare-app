2aabca8c5e11530722daf2f3c2d533baee793fed
Phil Ramirez <ramirezp6856@gmail.com>
2016-11-08 23:59:53 -0500

    Added PetCareApplication, Report, and StorePerformanceReport Classes.

---
+++ src/Report.java
@@ -0,0 +1,6 @@
+
+public abstract class Report {
+
+	abstract public void generateReport();
+
+}
---
+++ src/StorePerformanceReport.java
@@ -0,0 +1,15 @@
+
+public class StorePerformanceReport extends Report {
+
+	String providedMostServices;
+	String providedLeastServices;
+	Double weeklySales;
+	
+	@Override
+	public void generateReport() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
