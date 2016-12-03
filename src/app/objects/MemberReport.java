bfe840d0c582491fb9e4e9e75514c087aecd9664
usman-tahir <tahir.usman.ali94@outlook.com>
2016-12-02 21:55:50 -0500

    minor changes to the generateReport/AppProcess method signatures

--- src/app/objects/MemberReport.java
+++ src/app/objects/MemberReport.java
@@ -1,17 +1,18 @@
 package app.objects;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import app.utilities.Utility;
 
 public class MemberReport extends Report{
 	private ArrayList<Member> memberList;
 	private HashMap<String, TransactionObject> memberTransactions;
+	private int storeID;
 	private String report;
 
-	public MemberReport(ArrayList<Member> memberList, HashMap<String, TransactionObject> memberTransactions) {
+	public MemberReport(ArrayList<Member> memberList, HashMap<String, TransactionObject> memberTransactions, int storeID) {
 		this.memberList = memberList;
 		this.memberTransactions = memberTransactions;
+		this.storeID = storeID;
 		this.report = "";
 	}
 	
